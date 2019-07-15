3a6c8230564cfd783a96f20cba0d0e9c0c0f3ec5
AnuragDShetty <anuragshetty.1998@gmail.com>
2019-07-15 18:20:28 +0530

    Complete set up view of employee activities

--- app/src/main/java/com/example/android/employeeapp/EmpActivities.java
+++ app/src/main/java/com/example/android/employeeapp/EmpActivities.java
@@ -18,8 +18,9 @@ import android.widget.Toast;
 public class EmpActivities extends AppCompatActivity {
 
     ListView listView;
-    String names[]={"Manoj","Chetan","Achar"};
+    String names[]={"Manoj krishna","Chetan","Achar"};
     String descp[]={"Login","KMC","At office"};
+    String time[]={"12/07  09:18:25","14/07  10:18:25","14/07  11:20:25"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +28,7 @@ public class EmpActivities extends AppCompatActivity {
 
         listView=findViewById(R.id.activity_list);
 
-        emp_descpAdaptor adaptor=new emp_descpAdaptor(this,names,descp);
+        emp_descpAdaptor adaptor=new emp_descpAdaptor(this,names,descp,time);
         listView.setAdapter(adaptor);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -43,12 +44,14 @@ public class EmpActivities extends AppCompatActivity {
         Context context;
         String name[];
         String descp[];
+        String time[];
 
-        emp_descpAdaptor (Context c, String name[],String descp[]){
+        emp_descpAdaptor (Context c, String name[],String descp[],String time[]){
             super(c, R.layout.list_details,R.id.emp_name,name);
             this.context=c;
             this.name=name;
             this.descp=descp;
+            this.time=time;
 
         }
 
@@ -59,10 +62,10 @@ public class EmpActivities extends AppCompatActivity {
             View row=layoutInflater.inflate(R.layout.list_details,parent,false);
             TextView title=row.findViewById(R.id.emp_name);
             TextView desc=row.findViewById(R.id.emp_descp);
-
+            TextView t=row.findViewById(R.id.emp_time);
             title.setText(names[position]);
             desc.setText(descp[position]);
-
+            t.setText(time[position]);
             return row;
         }
     }
