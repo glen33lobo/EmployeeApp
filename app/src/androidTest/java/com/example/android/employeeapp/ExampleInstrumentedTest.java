1a9615717188db7a2b92cc493db97d962eebeec8
glen33lobo <glenlobo33@gmail.com>
2019-07-15 15:25:22 +0530

    All employees details

--- app/src/androidTest/java/com/example/android/employeeapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/android/employeeapp/ExampleInstrumentedTest.java
@@ -1,8 +1,8 @@
 package com.example.android.employeeapp;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
--- app/src/main/java/com/example/android/employeeapp/All_Employers.java
+++ app/src/main/java/com/example/android/employeeapp/All_Employers.java
@@ -1,48 +1,51 @@
 package com.example.android.employeeapp;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class All_Employers extends AppCompatActivity {
 
     RecyclerView recyclerView;
-    String[] BRANCH_NAME={"MANOJ","KRIHDNS","ASKKD"};
-    String[] Name_logo={"mj","ks","ac"};
+    RequestQueue requestQueue;
+    Integer ID[]=null;
+    String U_NAME[]=null;
+    String DESC[]=null;
+
+    String[] BRANCH_NAME={"MANOJ","KRIHDNS","ASKKD","dfsd","dfsdv"};
+    String[] Name_logo={"mj","ks","ac","grfer","gerg"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_employers);
+        requestQueue= Volley.newRequestQueue(this);
 
-        recyclerView=(RecyclerView)findViewById(R.id.employers_list);
+        performretrieval();
 
 
-        recyclerView.setHasFixedSize(true);
-
-        CustomAdapter radapter=new CustomAdapter(BRANCH_NAME,Name_logo);
-
-        RecyclerView.LayoutManager rlayoutmanager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
-
-        recyclerView.setLayoutManager(rlayoutmanager);
-        recyclerView.setAdapter(radapter);
-
-        radapter.setonItemclicklistener(new CustomAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(int pos) {
-//                Intent i=new Intent(All.this,Sub_Domain_list.class);
-//                i.putExtra("Position",pos);
-//                startActivity(i);
-                Toast.makeText(All_Employers.this, "Selected", Toast.LENGTH_SHORT).show();
-            }
-        });
 
 
     }
@@ -50,10 +53,95 @@ public class All_Employers extends AppCompatActivity {
 
 
 
+    public void performretrieval()
+    {
+        String url="http://www.thantrajna.com/sjec_task/Employee_details/Retrive_all_emp.php";
+        JsonObjectRequest name=new JsonObjectRequest( Request.Method.POST,url,null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                //JSONObject obj= null;
+                try {
+
+                    Toast.makeText(All_Employers.this, ""+response, Toast.LENGTH_SHORT).show();
+                    JSONArray jsonArray=response.getJSONArray("val");
+                    ID=new Integer[jsonArray.length()];
+                    U_NAME=new String[jsonArray.length()];
+                    DESC=new String[jsonArray.length()];
+                    for(int i=0;i<jsonArray.length();i++) {
+                        JSONObject obj = jsonArray.getJSONObject(i);
+                        ID[i] = obj.getInt("ID");
+                        U_NAME[i] = obj.getString("USERNAME");
+                        DESC[i] = obj.getString("DESCRIPTION");
+                    }
+
+                    Toast.makeText(All_Employers.this, U_NAME[0]+" "+U_NAME[1], Toast.LENGTH_SHORT).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+
+                recyclerView=(RecyclerView)findViewById(R.id.employers_list);
+                recyclerView.setHasFixedSize(true);
+                CustomAdapter radapter=new CustomAdapter(U_NAME,DESC);
+                RecyclerView.LayoutManager rlayoutmanager=new LinearLayoutManager(All_Employers.this,RecyclerView.VERTICAL,false);
+                recyclerView.setLayoutManager(rlayoutmanager);
+                recyclerView.setAdapter(radapter);
+
+                radapter.setonItemclicklistener(new CustomAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(int pos) {
+//                Intent i=new Intent(All.this,Sub_Domain_list.class);
+//                i.putExtra("Position",pos);
+//                startActivity(i);
+                        Toast.makeText(All_Employers.this, "Selected", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(All_Employers.this, "Err: " + error, Toast.LENGTH_SHORT).show();
+
+            }
+        })
+        {
+            @Override
+            protected Map<String,String> getParams()
+            {
+                Map<String,String> params=new HashMap<String, String>();
+//                params.put("uname",track_user);
+                return params;
+            }
+        };
+        requestQueue.add(name);
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ExampleViewHolder> {
 
         public String[] names_array;
-        public String[] logo_array;
+        public String[] desc_array;
 
         private OnItemClickListener mlister;
         public interface OnItemClickListener{
@@ -66,22 +154,22 @@ public class All_Employers extends AppCompatActivity {
         }
 
 
-        public CustomAdapter(String[] n_array,String[] l_array)
+        public CustomAdapter(String[] un_array,String[] desc_array)
         {
-            names_array=n_array;
-            logo_array=l_array;
+            this.names_array=un_array;
+            this.desc_array=desc_array;
 
         }
 
         public static class ExampleViewHolder extends RecyclerView.ViewHolder{
 
-            public TextView t_logo;
+            public TextView t_desc;
             public TextView t_name;
 
             public ExampleViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
                 super(itemView);
-                t_logo=(TextView)itemView.findViewById(R.id.id_name_logo);
                 t_name=(TextView)itemView.findViewById(R.id.id_name);
+                t_desc=(TextView)itemView.findViewById(R.id.descp);
 
                 itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -107,10 +195,10 @@ public class All_Employers extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull ExampleViewHolder viewHolder, int i) {
-            String tlogo=logo_array[i];
+            String ddesc=desc_array[i];
             String tname=names_array[i];
 
-            viewHolder.t_logo.setText(tlogo);
+            viewHolder.t_desc.setText(ddesc);
             viewHolder.t_name.setText(tname);
 
         }
@@ -118,7 +206,9 @@ public class All_Employers extends AppCompatActivity {
 
         @Override
         public int getItemCount() {
-            return 5;
+//            if(logo_array==null)
+//                return 0;
+            return desc_array.length;
         }
 
     }
